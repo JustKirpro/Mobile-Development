4d7f7dd9adb3f4edd9e63660023441c682e6f78f
Kirill Proskuryakov <justkirpro@gmail.com>
2023-03-02 21:35:52 +0500

    Add Swift tutorial

---
+++ Maps/Maps/MapsApp.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+@main
+struct MapsApp: App {
+    @StateObject private var modelData = ModelData()
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environmentObject(modelData)
+        }
+    }
+}
---
+++ Maps/Maps/Views/Landmarks/LandmarkList.swift
@@ -0,0 +1,38 @@
+import SwiftUI
+
+struct LandmarkList: View {
+    @EnvironmentObject var modelData: ModelData
+    @State private var showFavoritesOnly = false
+    
+    var filtredLandmarks: [Landmark] {
+        modelData.landmarks.filter {landmark in
+            (!showFavoritesOnly || landmark.isFavorite)
+        }
+    }
+    
+    var body: some View {
+        NavigationView {
+            List {
+                Toggle(isOn: $showFavoritesOnly) {
+                    Text("Favorites only")
+                }
+                
+                ForEach(filtredLandmarks) { landmark in
+                    NavigationLink {
+                        LandmarkDetail(landmark: landmark)
+                    } label: {
+                        LandmarkRow(landmark: landmark)
+                    }
+                }
+            }
+            .navigationTitle("Landmarks")
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+            .environmentObject(ModelData())
+    }
+}
